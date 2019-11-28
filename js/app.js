2447c94b2b9ded81990d84dc021ee274176f23df
Martin Cortes <martin.cortes@gmail.com>
2019-11-28 12:50:39 -0600

    Actualizacion

--- js/app.js
+++ js/app.js
@@ -13,7 +13,10 @@ if ( navigator.serviceWorker ) {
     navigator.serviceWorker.register( swLocation );
 }
 
-
+// $.getScript('js/app-utils.js', function() {
+//     console.log('Utiles cargados.');
+// });
+var totalcomanda = 0;
 
 var menu = {
         "comercio": "Las Lilas",
@@ -42,7 +45,7 @@ var menu = {
                 // imagen: file
             },
             {
-                id: 65656,
+                id: 65856,
                 categoria: ["postre"], 
                 plato: "Brownie",
                 ingredientes: "Dulce de leche y chocolate amargo",
@@ -62,49 +65,7 @@ var menu = {
 };
 
 
-function htmlPlato( plato ){
 
-        var contenido = `<div class="card card-body">
-        <div class="row justify-content-start">
-            <div class="col-10 col-md-9 plato " >
-                <div class="row justify-content-around">
-                    <div class="col-1 col-xs-1 col-sm-1 col-md-2 align-self-center text-center" >
-                        <button class="btn btn-primary row justify-content-start sumaplato" plato="${plato.id}">
-                            <i class="fa fa-plus-square"></i>
-                        </button>
-                    </div>
-                    <div class="col-10 col-xs-10 col-sm-11 col-md-10">
-                        <button class="btn btn-block nombreplato text-left" type="button" 
-                        data-toggle="collapse" data-target="#plato${ plato.id }"  
-                        href="#collapseExample" aria-expanded="false" aria-controls="collapseExample" >
-                            ${ plato.plato }
-                        </button>
-                        <div class="collapse detalleplato" id="plato${ plato.id }">
-                        ${ plato.ingredientes }
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-2 col-md-2 precio align-self-center text-right rt" >
-                $${ plato.precio }
-            </div>
-    `;
-    $("#comidas").append(contenido);
-
-}
-
-function getPlato (id){
-    var respuesta = false;
-    $.each(menu.menuitems, function(i, item) {
-        // console.log("Comparo "+id+" con "+item.id);
-        if(item.id == id){
-            respuesta = item;        
-            // return respuesta;
-        }  
-    });
-    // console.log("Retorno "+respuesta);
-    return respuesta;
-}
 
 
 $.each(menu.menuitems, function(i, item) {
@@ -114,7 +75,7 @@ $.each(menu.menuitems, function(i, item) {
 $(".sumaplato").click(function(){
     // alert("Busco "+$(this).attr("plato"));
     var plato = getPlato($(this).attr("plato"));
-    alert(plato.id);
+    agregaPlatoAComanda(plato.id);
 });
 
 // $("#prueba").append("asldasd");
