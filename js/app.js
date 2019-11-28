0c375323bbd5ab4af54f7d19dbc103d607ae5afc
Martin Cortes <martin.cortes@gmail.com>
2019-11-28 10:41:02 -0600

    Prueba

--- js/app.js
+++ js/app.js
@@ -1,6 +1,5 @@
-
 var url = window.location.href;
-var swLocation = '/twittor/sw.js';
+var swLocation = '/waiting/sw.js';
 
 
 if ( navigator.serviceWorker ) {
@@ -16,128 +15,90 @@ if ( navigator.serviceWorker ) {
 
 
 
+var menu = {
+        "comercio": "Las Lilas",
+        "menuitems": [ 
+            {
+                id: 2344,
+                categoria: ["almuerzo", "cena"], 
+                plato: "Pato a la naranja", 
+                ingredientes: "Pollo y hornoUn pato de 1.5 kg \
+                5 naranjas \
+                Un vaso de Cointreau \
+                1 cucharada sopera de vinagre \
+                1 cucharadita de mantequilla \
+                Un vaso de caldo de ave \
+                2 cucharaditas de harina de maíz \
+                una pizca de sal y pimienta negra.", 
+                precio: 34.5, 
+                // imagen: file
+            },
+            {
+                id: 65656,
+                categoria: ["postre"], 
+                plato: "Flan de dulce de leche",
+                ingredientes: "Flan y dulce de leche",
+                precio: 46,
+                // imagen: file
+            }
+        ]
+};
 
 
-// Referencias de jQuery
+function htmlPlato( plato ){
 
-var titulo      = $('#titulo');
-var nuevoBtn    = $('#nuevo-btn');
-var salirBtn    = $('#salir-btn');
-var cancelarBtn = $('#cancel-btn');
-var postBtn     = $('#post-btn');
-var avatarSel   = $('#seleccion');
-var timeline    = $('#timeline');
-
-var modal       = $('#modal');
-var modalAvatar = $('#modal-avatar');
-var avatarBtns  = $('.seleccion-avatar');
-var txtMensaje  = $('#txtMensaje');
-
-// El usuario, contiene el ID del héroe seleccionado
-var usuario;
-
-
-
-
-// ===== Codigo de la aplicación
-
-function crearMensajeHTML(mensaje, personaje) {
-
-    var content =`
-    <li class="animated fadeIn fast">
-        <div class="avatar">
-            <img src="img/avatars/${ personaje }.jpg">
-        </div>
-        <div class="bubble-container">
-            <div class="bubble">
-                <h3>@${ personaje }</h3>
-                <br/>
-                ${ mensaje }
+        var contenido = `<div class="card card-body">
+        <div class="row justify-content-start">
+            <div class="col-10 col-md-9 plato " >
+                <div class="row justify-content-around">
+                    <div class="col-1 col-xs-1 col-sm-1 col-md-2 align-self-center text-center" >
+                        <button class="btn btn-primary row justify-content-start sumaplato" plato="${plato.id}">
+                            <i class="fa fa-plus-square"></i>
+                        </button>
+                    </div>
+                    <div class="col-10 col-xs-10 col-sm-11 col-md-10">
+                        <button class="btn btn-block nombreplato text-left" type="button" 
+                        data-toggle="collapse" data-target="#plato${ plato.id }"  
+                        href="#collapseExample" aria-expanded="false" aria-controls="collapseExample" >
+                            ${ plato.plato }
+                        </button>
+                        <div class="collapse detalleplato" id="plato${ plato.id }">
+                        ${ plato.ingredientes }
+                        </div>
+                    </div>
+                </div>
             </div>
-            
-            <div class="arrow"></div>
-        </div>
-    </li>
+            <div class="col-2 col-md-2 precio align-self-center text-right rt" >
+                $${ plato.precio }
+            </div>
     `;
-
-    timeline.prepend(content);
-    cancelarBtn.click();
+    $("#comidas").append(contenido);
 
 }
 
-
-
-// Globals
-function logIn( ingreso ) {
-
-    if ( ingreso ) {
-        nuevoBtn.removeClass('oculto');
-        salirBtn.removeClass('oculto');
-        timeline.removeClass('oculto');
-        avatarSel.addClass('oculto');
-        modalAvatar.attr('src', 'img/avatars/' + usuario + '.jpg');
-    } else {
-        nuevoBtn.addClass('oculto');
-        salirBtn.addClass('oculto');
-        timeline.addClass('oculto');
-        avatarSel.removeClass('oculto');
-
-        titulo.text('Seleccione Personaje');
-    
-    }
-
-}
-
-
-// Seleccion de personaje
-avatarBtns.on('click', function() {
-
-    usuario = $(this).data('user');
-
-    titulo.text('@' + usuario);
-
-    logIn(true);
-
-});
-
-// Boton de salir
-salirBtn.on('click', function() {
-
-    logIn(false);
-
-});
-
-// Boton de nuevo mensaje
-nuevoBtn.on('click', function() {
-
-    modal.removeClass('oculto');
-    modal.animate({ 
-        marginTop: '-=1000px',
-        opacity: 1
-    }, 200 );
-
-});
-
-// Boton de cancelar mensaje
-cancelarBtn.on('click', function() {
-   modal.animate({ 
-       marginTop: '+=1000px',
-       opacity: 0
-    }, 200, function() {
-        modal.addClass('oculto');
-        txtMensaje.val('');
+function getPlato (id){
+    var respuesta = false;
+    $.each(menu.menuitems, function(i, item) {
+        // console.log("Comparo "+id+" con "+item.id);
+        if(item.id == id){
+            respuesta = item;        
+            // return respuesta;
+        }  
     });
+    // console.log("Retorno "+respuesta);
+    return respuesta;
+}
+
+
+$.each(menu.menuitems, function(i, item) {
+    htmlPlato(item);
 });
 
-// Boton de enviar mensaje
-postBtn.on('click', function() {
-
-    var mensaje = txtMensaje.val();
-    if ( mensaje.length === 0 ) {
-        cancelarBtn.click();
-        return;
-    }
-
-    crearMensajeHTML( mensaje, usuario );
-
+$(".sumaplato").click(function(){
+    // alert("Busco "+$(this).attr("plato"));
+    var plato = getPlato($(this).attr("plato"));
+    alert(plato.id);
 });
+
+// $("#prueba").append("asldasd");
+// $("#prueba").append(menu.menu);
